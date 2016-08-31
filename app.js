6b917f81738f0b9a513fc3731481a33a34fadd7f
Joey Hobbs <joey@goodeggs.com>
2016-08-31 15:36:22 -0700

    Drying up a little

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 import express from 'express'
 
+import middleware from './infra/middleware'
 import controller from './web/controller'
 
 const app = express()
-
-app.use((req, res, next) => {
-  req.logger = console
-  req.metrics = (metric) => console.log('metric:', metric)
-  next()
-})
-
+app.use(middleware)
 app.get('/add_product', controller)
 
 export default app
