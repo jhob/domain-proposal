f6ac6e04981f697ad7914519d0bd9d0410dd7b63
Joey Hobbs <joey@goodeggs.com>
2016-08-31 13:37:42 -0700

    Some code tidy-up

--- util/domain.js
+++ util/domain.js
@@ -1,7 +1,15 @@
 const internalErrorTypes = ['INVALID_ERROR_MESSAGE', 'INVALID_ERROR_TYPE']
 
-export default function domain ({name, actions, errorTypes}) {
-  const makeError = ({type, message, details}) => {
+
+function bindActions (actions, context) {
+  return Object.keys(actions).reduce((out, actionName) => {
+    out[actionName] = actions[actionName].bind(context)
+    return out
+  }, {})
+}
+
+function getMakeError(name, errorTypes) {
+  return function makeError ({type, message, details}) {
     if (errorTypes.indexOf(type) === -1 && internalErrorTypes.indexOf(type) === -1) {
       throw makeError({
         type: 'INVALID_ERROR_TYPE',
@@ -20,14 +28,16 @@ export default function domain ({name, actions, errorTypes}) {
     error.message = message
     return error
   }
+}
 
+export default function domain ({name, actions, errorTypes}) {
   return ({logger, metrics}) => {
-    const context = {logger, metrics, makeError, cache: {}}
-    const boundActions = Object.keys(actions).reduce((out, actionName) => {
-      out[actionName] = actions[actionName].bind(context)
-      return out
-    }, {})
-    context.actions = boundActions
-    return boundActions
+    const context = {
+      cache: {},
+      logger,
+      makeError: getMakeError(name, errorTypes),
+      metrics
+    }
+    return context.actions = bindActions(actions, context)
   }
 }
